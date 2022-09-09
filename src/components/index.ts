defb426ffdf0bdf22f05a7710418aa8c7ca7c40f
Romario Negreiros <nromario482@gmail.com>
2022-09-09 18:24:41 -0300

    Export new loader component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as RegisterForm } from './RegisterForm'
 export { default as UnregisterForm } from './UnregisterForm'
 export { default as UpdateForm } from './UpdateForm'
+export { default as Loader } from './Loader'
