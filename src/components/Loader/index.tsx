e97386f530e2442dd3e3ea67bd54916a10730c17
Romario Negreiros <nromario482@gmail.com>
2022-09-09 18:24:31 -0300

    Create loader spinner styles and component

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react'
+
+import styles from './styles.module.css'
+
+const Loading: FC = () => {
+  return (
+    <div className={styles.loader}>
+      <div>
+      </div>
+    </div>
+  )
+}
+
+export default Loading
