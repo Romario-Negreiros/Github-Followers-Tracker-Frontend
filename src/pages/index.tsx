7a5ca5b7117428b0309414afe6cff80eeaec70ce
Romario Negreiros <nromario482@gmail.com>
2022-09-09 22:39:28 -0300

    Create setter and handler methods, pass props and handle states
    Create setters to change value of either error, successMsg or isLoaded states and add or remove a classList named overflow-hidden in the body element to avoid overflow when the states will be shown.
    Create handleFormSelect method to update the form state value.
    Render conditionally a Loader, a Error, and a div with the successMsg (a Success component will be created).
    Pass setter methods as props to all form components that will be rendered inside RenderForm method.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,79 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 
-import { RegisterForm, UnregisterForm, UpdateForm } from '../components'
+import { RegisterForm, UnregisterForm, UpdateForm, Loader, Error } from '../components'
 
 import type { NextPage } from 'next'
 
 type Forms = 'register' | 'unregister' | 'update'
 
 const Home: NextPage = () => {
-  const [form, setForm] = useState<Forms>('unregister')
+  const [isLoaded, setIsLoaded] = useState(true)
+  const [error, setError] = useState('')
+  const [successMsg, setSuccessMsg] = useState('')
+  const [form, setForm] = useState<Forms>('register')
 
-  const renderForm = () => {
-    switch (form) {
-      case 'register':
-        return <RegisterForm />
-      case 'unregister':
-        return <UnregisterForm />
-      case 'update':
-        return <UpdateForm />
+  const setErrorState = (error: string) => {
+    if (error) {
+      document.body.classList.add('overflow-hidden')
+    } else {
+      document.body.classList.remove('overflow-hidden')
     }
+    setError(error)
+  }
+
+  const setIsLoadedState = (isLoaded: boolean) => {
+    if (isLoaded) {
+      document.body.classList.remove('overflow-hidden')
+    } else {
+      document.body.classList.add('overflow-hidden')
+    }
+    setIsLoaded(isLoaded)
+  }
+
+  const setSuccessMsgState = (successMsg: string) => {
+    if (successMsg) {
+      document.body.classList.add('overflow-hidden')
+    } else {
+      document.body.classList.remove('overflow-hidden')
+    }
+    setSuccessMsg(successMsg)
   }
 
   const handleFormSelect = (selectedForm: Forms) => {
     setForm(selectedForm)
   }
 
+  const renderForm = () => {
+    switch (form) {
+      case 'register':
+        return <RegisterForm setErrorState={setErrorState} setIsLoadedState={setIsLoadedState} setSuccessMsgState={setSuccessMsgState} />
+      case 'unregister':
+        return <UnregisterForm setErrorState={setErrorState} setIsLoadedState={setIsLoadedState} setSuccessMsgState={setSuccessMsgState} />
+      case 'update':
+        return <UpdateForm setErrorState={setErrorState} setIsLoadedState={setIsLoadedState} setSuccessMsgState={setSuccessMsgState} />
+    }
+  }
+
   return (
     <main>
       <h1>Github Followers Tracker</h1>
       <ul className="form-selection">
-        <li onClick={() => handleFormSelect('register')} className={form === 'register' ? 'form-active' : ''}>Register</li>
-        <li onClick={() => handleFormSelect('unregister')} className={form === 'unregister' ? 'form-active' : ''}>Unregister</li>
-        <li onClick={() => handleFormSelect('update')} className={form === 'update' ? 'form-active' : ''}>Update</li>
+        <li onClick={() => handleFormSelect('register')} className={form === 'register' ? 'form-active' : ''}>
+          Register
+        </li>
+        <li onClick={() => handleFormSelect('unregister')} className={form === 'unregister' ? 'form-active' : ''}>
+          Unregister
+        </li>
+        <li onClick={() => handleFormSelect('update')} className={form === 'update' ? 'form-active' : ''}>
+          Update
+        </li>
       </ul>
       {renderForm()}
+      {(!isLoaded || error || successMsg) && (
+        <div className="cover">
+          {!isLoaded ? <Loader /> : error ? <Error error={error} /> : successMsg ? <div className="successMsg">{successMsg}</div> : ''}
+        </div>
+      )}
     </main>
   )
 }
