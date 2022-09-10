b3a502445ddc59571e24755c0ea1cc9499ed2736
Romario Negreiros <nromario482@gmail.com>
2022-09-09 22:35:04 -0300

    Add type of setSuccessMsgState method of FormComponentsProps interface

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,4 +1,5 @@
 export default interface FormComponentsProps {
   setErrorState: (error: string) => void
   setIsLoadedState: (isLoaded: boolean) => void
+  setSuccessMsgState: (successMsg: string) => void
 }
