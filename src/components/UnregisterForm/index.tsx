aa0cfc651a660547cf9c228435b717041c7cc636
Romario Negreiros <nromario482@gmail.com>
2022-09-11 20:08:11 -0300

    Integrate with react hook form, create onSubmit function and handle states
    Use setters received through props to handle error, isLoaded or successMsg states.

--- src/components/UnregisterForm/index.tsx
+++ src/components/UnregisterForm/index.tsx
@@ -1,6 +1,46 @@
-import { FC } from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
 
-const UnregisterForm: FC = () => {
+// Email pattern regEx
+import EmailPattern from '@common/regExs'
+
+import type FormComponentsProps from '@common/types'
+import type { FC } from 'react'
+
+interface Input {
+  email: string
+}
+
+const UnregisterForm: FC<FormComponentsProps> = ({ setErrorState, setIsLoadedState, setSuccessMsgState }) => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors }
+  } = useForm<Input>()
+
+  const onSubmit: SubmitHandler<Input> = async data => {
+    try {
+      setIsLoadedState(false)
+      const response = await fetch('http://localhost:3000/users/unregister', {
+        method: 'DELETE',
+        body: JSON.stringify(data),
+        headers: new Headers({
+          'Content-Type': 'application.json'
+        })
+      })
+      const body = await response.json()
+      if (response.ok) {
+        setSuccessMsgState(body)
+      } else {
+        throw new Error(body.error)
+      }
+    } catch (err) {
+      if (err instanceof Error) {
+        setErrorState(err.message)
+      }
+    } finally {
+      setIsLoadedState(true)
+    }
+  }
   return (
     <div className="form-container">
       <h2>Unregister</h2>
@@ -8,8 +48,21 @@ const UnregisterForm: FC = () => {
         This will stop the tracking of your github profile&apos;s followers and you will no longer receive a daily
         report in your email!
       </p>
-      <form>
-        <input type="email" placeholder="Email..."></input>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          {errors.email?.message && <p className="error">{errors.email.message}</p>}
+          <input
+            type="email"
+            placeholder="Email..."
+            {...register('email', {
+              required: 'Your email is needed!',
+              pattern: {
+                value: EmailPattern,
+                message: 'Please input a valid email!'
+              }
+            })}
+          ></input>
+        </div>
         <button type="submit">Unregister</button>
       </form>
     </div>
