80f04bb95f01013684b12e8643490193c1eefd85
Romario Negreiros <nromario482@gmail.com>
2022-09-10 22:03:55 -0300

    Add CloseModal component and pass corresponding setter method as props

--- src/components/Error/index.tsx
+++ src/components/Error/index.tsx
@@ -1,14 +1,20 @@
 import { FC } from 'react'
 
+import { CloseModal } from '..'
+
 import styles from './styles.module.css'
+
+import type FormComponentsProps from '@common/types'
 
 interface Props {
   error: string
+  setErrorState: FormComponentsProps['setErrorState']
 }
 
-const Error: FC<Props> = ({ error }) => {
+const Error: FC<Props> = ({ error, setErrorState }) => {
   return (
     <div className={styles.container}>
+      <CloseModal setState={setErrorState} />
       <h3>Hmm, something went wrong...</h3>
       <p>{error}</p>
     </div>
--- src/components/Success/index.tsx
+++ src/components/Success/index.tsx
@@ -1,14 +1,19 @@
+import CloseModal from '../CloseModal'
+
 import styles from './styles.module.css'
 
 import type { FC } from 'react'
+import type FormComponentsProps from '@common/types'
 
 interface Props {
   successMsg: string
+  setSuccessMsgState: FormComponentsProps['setSuccessMsgState']
 }
 
-const Success: FC<Props> = ({ successMsg }) => {
+const Success: FC<Props> = ({ successMsg, setSuccessMsgState }) => {
   return (
     <div className={styles.container}>
+      <CloseModal setState={setSuccessMsgState} />
       <h3>Done!</h3>
       <p>{successMsg}</p>
     </div>
